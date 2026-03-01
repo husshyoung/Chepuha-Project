10c299b6a2876c04a142cf9f6172c3000d8ccebe
husshyoung <savchenkooleksejj@gmail.com>
2026-03-01 20:04:08 +0200

    Fix: setup Strapi connection to Supabase

--- chepuhaBackend/config/database.js
+++ chepuhaBackend/config/database.js
@@ -1,14 +1,13 @@
-const path = require('path');
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'Chepuha'),
+      database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '11357'),
-      ssl: env.bool('DATABASE_SSL', false),
+      password: env('DATABASE_PASSWORD'),
+      ssl: { rejectUnauthorized: false },
     },
     debug: false,
   },
